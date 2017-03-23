809aec4e0649d3edc244179f997c6f279e7cc624
Ian Wordes <iwordes@student.42.us.org>
2017-03-22 18:28:41 -0700

    Update check.c

--- src/check.c
+++ src/check.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 13:03:57 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/22 15:41:17 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/22 16:32:13 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ bool	check(t_stack *a, t_stack *b)
 	i = 1;
 	while (i < a->len)
 	{
-		if (PREV < CURR)
+		if (CURR > PREV)
 			return (false);
 		i += 1;
 	}
