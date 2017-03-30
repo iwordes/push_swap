0f5f3df5969a12a55ba8bd663f72a3f03e25e85b
Ian Wordes <iwordes@student.42.us.org>
2017-03-30 15:52:46 -0700

    State commit

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 14:00:35 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/30 15:12:29 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/30 15:47:04 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,22 @@
 #define B b->arr
 #define S s.arr
 
-#define A1 A[a->len - 1]
-#define A2 A[a->len - 2]
-#define B1 B[b->len - 1]
-#define B2 B[b->len - 2]
-#define S1 S[s.len - 1]
-#define S2 S[s.len - 2]
+#define TA A[a->len - 1]
+#define TB B[b->len - 1]
 
 #define OP(O) op__(#O, op_##O, a, b)
 
 #define CUR A[(o < 0) ? ~o : a->len - 1 - o]
-#define ROTOPT (mo > H2(a->len) ? mo - a->len : mo)
-#define SYNT_HL_BOUND
 
-static int	find(t_stack *a)
+static int	find(t_stack *a, t_stack *b)
 {
 	int		o;
 
 	o = 0;
 	while (ABS(o) < H2(a->len))
 	{
-		if (CUR < mn)
-			return (o);
+		// ...
+		(void)b;
 		o = -o + (o <= 0);
 	}
 	return (INT_MIN);
@@ -53,13 +47,8 @@ void		sort(t_stack *a, t_stack *b)
 
 	while (a->len > 1)
 	{
-		o = find(a);
-		if (o < 0)
-			while (o++)
-				OP(rra);
-		else
-			while (o--)
-				OP(ra);
+		o = find(a, b);
+		op__srot(a, o, true);
 		OP(pb);
 	}
 	while (b->len != 0)
