ca4a4070c1ffdbd408a9e278ad2d29a1c8a44aa2
Ian Wordes <iwordes@student.42.us.org>
2017-03-30 18:40:31 -0700

    Show stacks top-to-bottom rather than bottom-to-top

--- src/show.c
+++ src/show.c
@@ -6,11 +6,14 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 17:38:15 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/29 19:03:27 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/30 18:39:54 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
+
+#define A a->arr[a->len - i - 1]
+#define B b->arr[b->len - i - 1]
 
 void	show(t_stack *a, t_stack *b)
 {
@@ -20,16 +23,16 @@ void	show(t_stack *a, t_stack *b)
 	i = 0;
 	while (i < a->len)
 	{
-		ft_printf("%s%d", (i > 0) ? ", " : "", a->arr[i]);
+		ft_printf("%s%d", (i > 0) ? ", " : "", A);
 		i += 1;
 	}
-	ft_putstr("] <=\n");
+	ft_putstr("]\n");
 	ft_putstr("\e[94mB[");
 	i = 0;
 	while (i < b->len)
 	{
-		ft_printf("%s%d", (i > 0) ? ", " : "", b->arr[i]);
+		ft_printf("%s%d", (i > 0) ? ", " : "", B);
 		i += 1;
 	}
-	ft_putstr("] <=\e[0m\n\n");
+	ft_putstr("]\e[0m\n\n");
 }
