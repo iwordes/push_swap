334b58454803f232dd060a68f90426dcca30eb2b
Ian Wordes <iwordes@student.42.us.org>
2017-04-18 11:33:29 -0700

    State commit

--- src/sort/sort2.c
+++ src/sort/sort2.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/16 19:45:21 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/18 10:43:59 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/18 11:32:21 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	cost(t_stack *a, int o, int n)
 
 	r2 = 0;
 	seen = 0;
-	while (r2 < a->len && n >= A[-I])
+	while (r2 < a->len && n > A[-I])
 	{
 		if (n == A[I])
 			seen = 1;
@@ -37,11 +37,15 @@ static int	cost(t_stack *a, int o, int n)
 	return (o + r2 - seen);
 }
 
-#define NEW (unsigned)(ABS(OPT(r1)) + ABS(OPT(r2)))
+#define AL1 a->len
+#define AL2 (a->len - 1)
+#define OPT1(K) (K % AL1) - (((K % AL1) > AL1 / 2) ? AL1 : 0)
+#define OPT2(K) (K % AL2) - (((K % AL2) > AL2 / 2) ? AL2 : 0)
+
+#define NEW (unsigned)(ABS(OPT1(r1)) + ABS(OPT2(r2)))
 #define OLD (unsigned)(ABS(*rot1) + ABS(*rot2))
 
-#define OPM(K) (K % a->len)
-#define OPT(K) OPM(K) - ((OPM(K) > a->len / 2) ? a->len : 0)
+
 
 static void	best(t_stack *a, int *rot1, int *rot2)
 {
@@ -49,6 +53,9 @@ static void	best(t_stack *a, int *rot1, int *rot2)
 	int		r2;
 	int		o;
 
+	//static int	dump = 0;
+
+	ft_eprintf("\e[96mbest\e[0m\n");
 	o = 0;
 	r1 = 0;
 	r2 = 0;
@@ -56,16 +63,27 @@ static void	best(t_stack *a, int *rot1, int *rot2)
 	*rot2 = INT_MAX;
 	while (A[-o] != a->min)
 		o += 1;
+	ft_eprintf("  o: %d\n", o);
 	while (r1 < a->len)
 	{
 		r2 = cost(a, o, A[-r1]) - r1;
-		if (NEW < OLD)
+		ft_eprintf("  %d, %d (%d, %d)\n", r1, r2, OPT1(r1), OPT2(r2));
+		if (A[-r1] != a->min && OPT2(r2) != 0 && NEW < OLD)
 		{
-			*rot1 = OPT(r1);
-			*rot2 = OPT(r2);
+			ft_eprintf("  \e[92m^\e[0m\n");
+			*rot1 = OPT1(r1);
+			*rot2 = OPT2(r2);
 		}
+
+		//for (unsigned wait = ~0; wait > 5; wait -= 5);
+
 		r1 += 1;
 	}
+
+	//for (unsigned wait = ~0; wait > 5; wait -= 2);
+
+	//if (dump++ == -1)
+	//	while(1);
 }
 
 void	sort2(t_stack *a, t_stack *b)
@@ -77,11 +95,14 @@ void	sort2(t_stack *a, t_stack *b)
 		return ;
 	while (check_asc(a) == INT_MIN)
 	{
+		//show(a, b, "");
+
 		best(a, &r1, &r2);
 		op__srot(a, r1, 'a');
 		OP(pb);
 		op__srot(a, r2, 'a');
 		OP(pa);
 	}
+	ft_putstr("ss\nss\n");
 	while(1);
 }
