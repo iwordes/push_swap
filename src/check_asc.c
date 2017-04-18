ccf45a1a73ec7897fd8cc3e277a548d1d4998cc0
Ian Wordes <iwordes@student.42.us.org>
2017-04-18 13:43:40 -0700

    Update check_*

--- src/check_asc.c
+++ src/check_asc.c
@@ -6,16 +6,15 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/15 20:06:25 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/16 14:59:05 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/18 12:48:57 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-#define S s->arr
-#define I(IX) (s->len - 1 - ((o + IX) % s->len))
-#define I1 I(i)
-#define I2 I(i + 1)
+#define S (s->arr + s->len - 1)
+#define I1 ((o + i) % s->len)
+#define I2 ((o + i + 1) % s->len)
 
 int		check_asc(t_stack *s)
 {
@@ -24,11 +23,11 @@ int		check_asc(t_stack *s)
 
 	i = 0;
 	o = 0;
-	while (S[I1] != s->min)
+	while (S[-o] != s->min)
 		o += 1;
 	while (i + 1 < s->len)
 	{
-		if (S[I1] > S[I2])
+		if (S[-I1] > S[-I2])
 			return (INT_MIN);
 		i += 1;
 	}
